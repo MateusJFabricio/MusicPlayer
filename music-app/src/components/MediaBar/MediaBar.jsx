ec5416c3ff9ffd1c64671cc6d3493a5f0dfb00ff
Mateus Fabricio <fabriciomateus05@gmail.com>
2023-08-24 18:33:16 +0200

    Adicionado comandos de Play/Pause na musica

--- music-app/src/components/MediaBar/MediaBar.jsx
+++ music-app/src/components/MediaBar/MediaBar.jsx
@@ -1,14 +1,74 @@
-import React, { useState } from "react";
+import React, { useState,useContext, useEffect, createRef } from "react";
 import "./MediaBar.css";
 import nextIcon from "../../assets/next.png"
 import playIcon from "../../assets/play.png"
+import pauseIcon from "../../assets/pause.png"
 import MusicDetails from "../MusicDetails/MusicDetails";
+import {MusicContext} from '../../context/MusicContext'
 
 const MediaBar = () => {
-  const [actualMusic, setActualMusic] = useState()
+  const URL_API = "http://localhost:3000/"
+  const [audio, setAudio] = useState()
+  const [playing, setPlaying] = useState(false)
+  const {musicStack, setMusicStack} = useContext(MusicContext)
+  let musicUrl = null
+  const audioRef = createRef();
+
+  const loadMusicStream = async (id)=>{
+    try {
+      const blob = await fetch(URL_API + "music/loadmusic/" + id)
+      .then(response=> response.blob())
+
+      musicUrl = URL.createObjectURL(blob)
+      return musicUrl
+      
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+
+  const handlePlayPause = async()=>{
+    if (!musicStack || musicStack.lenght === 0){
+      return
+    }
+    
+    if (!audio){
+      const aux = await loadMusicStream(musicStack[0]._id)
+      setAudio(aux)
+      return 
+    }
+    if (!playing){
+      handlePlay()
+    }else{
+      handlePause()
+    }
+  }
+
+  const handlePlay = () => {
+    audioRef.current.play()
+    setPlaying(true)
+  }
+
+  const handlePause = () => {
+    audioRef.current.pause()
+    setPlaying(false)
+  }
+
+  const handleTimeUpdate = () => {
+    // this.setState({
+    //   currentTime: this.audioRef.current.currentTime,
+    //   duration: this.audioRef.current.duration
+    // })
+  }
+
+  const handleMusicEnded = ()=>{
+    console.log("A musica acabou")
+  }
 
   return (
     <div className="mediabar-container">
+      <audio ref={audioRef} onTimeUpdate={handleTimeUpdate} onEnded={handleMusicEnded} src={audio} autoPlay controls/>
       {/* Media bar progress */}
       <div className="mediabar-progressinfo">
         <div className="mediabar-infoinit">02:56</div>
@@ -22,11 +82,11 @@ const MediaBar = () => {
       {/* Media bar controls */}
       <div className="music-controls">
         <div className="music-details">
-            <MusicDetails music={actualMusic}/>
+            <MusicDetails music={musicStack[0]}/>
         </div>
         <div className="controls">
           <div className="play">
-            <img alt="Play Music" src={playIcon}/>
+          <img onClick={handlePlayPause} alt="Play Music" src={playing?pauseIcon:playIcon}/>
           </div>
           <div className="next">
             <div className="overlap-group-2">
